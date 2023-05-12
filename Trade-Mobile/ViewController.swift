f2c03ad5548fca7b07ac3899aa795d7a6348645c
ArturKondratev <a.kondrat473@gmail.com>
2023-05-12 18:54:57 +0300

    Initial Commit

---
+++ Trade-Mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trade-Mobile
+//
+//  Created by Артур Кондратьев on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
